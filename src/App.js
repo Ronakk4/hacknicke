0fb379396a52a51cda161cd3822ba6cf0a335e9d
ronakk4 <ronakkjaveri15@gmail.com>
2023-01-18 23:08:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Timeline from './components/Timeline';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Timeline/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
